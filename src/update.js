3531875a7e07424ca12b2a59285d62ec7362759e
linwaru <linwaru@outlook.com>
2025-03-12 00:49:41 -0300

    update

--- src/update.js
+++ src/update.js
@@ -1,28 +1,43 @@
 const path = require('path');
 const fetch = require('node-fetch');
-const fs = require('fs');
+const fs = require('fs').promises;
+
+const getDateSuffix = (day) => {
+    if (day > 3 && day < 21) return 'th';
+    switch (day % 10) {
+        case 1: return 'st';
+        case 2: return 'nd';
+        case 3: return 'rd';
+        default: return 'th';
+    }
+};
 
 (async () => {
-    //Get ReadMe path
-    const ReadMe = path.join(__dirname, '..', 'README.md')
-    const date = new Date()
+    const ReadMe = path.join(__dirname, '..', 'README.md');
+    const date = new Date();
 
-    //Fetching Info From Github API
-    let UserData = await fetch('https://api.github.com/users/linwaru').then(res => res.json())
+    let UserData;
+    try {
+        const response = await fetch('https://api.github.com/users/linwaru');
+        if (!response.ok) throw new Error('Failed to fetch user data');
+        UserData = await response.json();
+    } catch (error) {
+        console.error('Error fetching user data:', error);
+        UserData = { followers: 'N/A', following: 'N/A' };
+    }
 
-    //Creating the text what we gonna save on ReadMe file
+    const tools = [
+        { name: "bash", link: "https://www.gnu.org/software/bash/", icon: "https://www.vectorlogo.zone/logos/gnu_bash/gnu_bash-icon.svg" },
+        { name: "bootstrap", link: "https://getbootstrap.com", icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg" },
+    ];
+
+    const toolIcons = tools.map(tool => `<a href="${tool.link}" target="_blank"><img src="${tool.icon}" alt="${tool.name}" width="40" height="40"/></a>`).join(' ');
+
+    const dateSuffix = getDateSuffix(date.getDate());
     const text = `<!-- You found this secret 👏 -->
-<!--
-    My secret things lol
-    
-    - I code more hours 
-    - I am a gamer too 
-    - I play Minecraft, Grand Fantasia, Strinova, Transformice and much more
-    - This readme.md is created using GitHub Codespaces 👀
--->
     
 <h1 align="center">Who Am I?</h1>
-    
+
 ## Hi, I'm Lin <img src="https://raw.githubusercontent.com/linwaru/linwaru/main/images/WaveIcon.gif" width="30px">
 
 \`\`\`js
@@ -30,10 +45,11 @@ const Linwaru = {
     description: "I am a developer of web projects and programming applications using Js, Ts, Rust and Axulogic, Inc. Founder",
     favouriteLanguage: "Javascript/Typescript/Rust",
     favouriteThings: ["Anime", "Games", "Music", "Programming"],
-}; //I'm a simple person who likes to program and play games
+};
 \`\`\`
+
 <h2 align="left">Language and Tools:</h2>
-<p align="left"> <a href="https://www.gnu.org/software/bash/" target="_blank"> <img src="https://www.vectorlogo.zone/logos/gnu_bash/gnu_bash-icon.svg" alt="bash" width="40" height="40"/> </a> <a href="https://getbootstrap.com" target="_blank"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg" alt="bootstrap" width="40" height="40"/> </a> <a href="https://canvasjs.com" target="_blank"> <img src="https://raw.githubusercontent.com/Hardik0307/Hardik0307/master/assets/canvasjs-charts.svg" alt="canvasjs" width="40" height="40"/> </a> <a href="https://www.w3schools.com/css/" target="_blank"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg" alt="css3" width="40" height="40"/> </a> <a href="https://www.docker.com/" target="_blank"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg" alt="docker" width="40" height="40"/> </a> <a href="https://expressjs.com" target="_blank"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg" alt="express" width="40" height="40"/> </a> <a href="https://www.figma.com/" target="_blank"> <img src="https://www.vectorlogo.zone/logos/figma/figma-icon.svg" alt="figma" width="40" height="40"/> </a> <a href="https://firebase.google.com/" target="_blank"> <img src="https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg" alt="firebase" width="40" height="40"/> </a> <a href="https://www.w3.org/html/" target="_blank"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg" alt="html5" width="40" height="40"/> </a> <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" alt="javascript" width="40" height="40"/> </a> <a href="https://www.mongodb.com/" target="_blank"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg" alt="mongodb" width="40" height="40"/> </a> <a href="https://www.nginx.com" target="_blank"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nginx/nginx-original.svg" alt="nginx" width="40" height="40"/> </a> <a href="https://nodejs.org" target="_blank"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg" alt="nodejs" width="40" height="40"/> </a> <a href="https://reactjs.org/" target="_blank"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg" alt="react" width="40" height="40"/> </a> <a href="https://sass-lang.com" target="_blank"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/sass/sass-original.svg" alt="sass" width="40" height="40"/> </a> <a href="https://tailwindcss.com/" target="_blank"> <img src="https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg" alt="tailwind" width="40" height="40"/> </a> <a href="https://www.typescriptlang.org/" target="_blank"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg" alt="typescript" width="40" height="40"/> </a> <a href="https://unity.com/" target="_blank"> <img src="https://www.vectorlogo.zone/logos/unity3d/unity3d-icon.svg" alt="unity" width="40" height="40"/> </a> </p>
+<p align="left">${toolIcons}</p>
 
 ## Stats
 <div align="center"><img src="https://github-profile-trophy.vercel.app/?username=linwaru&theme=dracula"></div>
@@ -43,12 +59,12 @@ const Linwaru = {
     
 <details>
     <summary><b>GitHub</b> Activity</summary>
-    <img align="left" src="https://github-readme-stats.vercel.app/api?username=linwaru&theme=tokyonight"><img align="right" src="https://github-readme-stats.vercel.app/api/top-langs/?username=linwaru&theme=tokyonight&hide=batchfile">
+    <img align="left" src="https://github-readme-stats.vercel.app/api?username=linwaru&theme=tokyonight">
+    <img align="right" src="https://github-readme-stats.vercel.app/api/top-langs/?username=linwaru&theme=tokyonight&hide=batchfile">
     <img src="https://github-readme-streak-stats.herokuapp.com/?user=linwaru&theme=tokyonight">
 </details>
-<!-- Last updated on ${date.toString()} -->
-<i>Last updated on ${date.getDate()}${date.getDate()===1?"st":date.getDate()===2?"nd":date.getDate()===3?"rd":"th"} ${["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"][date.getMonth()]} ${date.getFullYear()} using magic</i> ✨`
 
-    //Saving on readme.md
-    fs.writeFileSync(ReadMe, text)
-})()
+<i>Last updated on ${date.getDate()}${dateSuffix} ${["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"][date.getMonth()]} ${date.getFullYear()} using magic</i> ✨`;
+
+    await fs.writeFile(ReadMe, text);
+})();
