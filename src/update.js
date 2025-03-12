e6dd244b885efb9dd9648380183b1f5c36e4c95d
linwaru <linwaru@outlook.com>
2025-03-12 01:19:15 -0300

    [Automatic] Updated ReadMe.md

--- src/update.js
+++ src/update.js
@@ -6,17 +6,7 @@ const fs = require('fs');
     const ReadMe = path.join(__dirname, '..', 'README.md');
     const date = new Date();
 
-    const brasiliaOffset = -3 * 60;
-    const localDate = new Date(date.getTime() + brasiliaOffset * 60 * 1000);
-
-    const readmeContent = fs.readFileSync(ReadMe, 'utf-8');
-    const regex = /Last updated on (\d{1,2})(st|nd|rd|th)? (\w+) (\d{4}) at (\d{1,2}):(\d{2})(?::(\d{2}))?/;
-    const match = readmeContent.match(regex);
-
-    if (match && match[1] == localDate.getDate() && match[3] == ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"][localDate.getMonth()]) {
-        console.log('README.md is already up to date!');
-        return;
-    }
+    const brazilianTime = new Date(date.toLocaleString("en-US", { timeZone: "America/Sao_Paulo" }));
 
     let UserData = await fetch('https://api.github.com/users/linwaru').then(res => res.json());
 
@@ -38,8 +28,8 @@ const fs = require('fs');
 const Linwaru = {
     description: "I am a developer of web projects and programming applications using Js, Ts, Rust and Axulogic, Inc. Founder",
     favouriteLanguage: "Javascript/Typescript/Rust",
-    favouriteThings: ["Animes", "Games", "Music", "Programming"],
-}; //I'm a simple person who likes to program and play games
+    favouriteThings: ["Anime", "Games", "Music", "Programming"],
+}; // I'm a simple person who likes to program and play games
 \`\`\`
 <h2 align="left">Language and Tools:</h2>
 <p align="left"> <a href="https://www.gnu.org/software/bash/" target="_blank"> <img src="https://www.vectorlogo.zone/logos/gnu_bash/gnu_bash-icon.svg" alt="bash" width="40" height="40"/> </a> <a href="https://getbootstrap.com" target="_blank"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg" alt="bootstrap" width="40" height="40"/> </a> <a href="https://canvasjs.com" target="_blank"> <img src="https://raw.githubusercontent.com/Hardik0307/Hardik0307/master/assets/canvasjs-charts.svg" alt="canvasjs" width="40" height="40"/> </a> <a href="https://www.w3schools.com/css/" target="_blank"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg" alt="css3" width="40" height="40"/> </a> <a href="https://www.docker.com/" target="_blank"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg" alt="docker" width="40" height="40"/> </a> <a href="https://expressjs.com" target="_blank"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg" alt="express" width="40" height="40"/> </a> <a href="https://www.figma.com/" target="_blank"> <img src="https://www.vectorlogo.zone/logos/figma/figma-icon.svg" alt="figma" width="40" height="40"/> </a> <a href="https://firebase.google.com/" target="_blank"> <img src="https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg" alt="firebase" width="40" height="40"/> </a> <a href="https://www.w3.org/html/" target="_blank"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg" alt="html5" width="40" height="40"/> </a> <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" alt="javascript" width="40" height="40"/> </a> <a href="https://www.mongodb.com/" target="_blank"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg" alt="mongodb" width="40" height="40"/> </a> <a href="https://www.nginx.com" target="_blank"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nginx/nginx-original.svg" alt="nginx" width="40" height="40"/> </a> <a href="https://nodejs.org" target="_blank"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg" alt="nodejs" width="40" height="40"/> </a> <a href="https://reactjs.org/" target="_blank"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg" alt="react" width="40" height="40"/> </a> <a href="https://sass-lang.com" target="_blank"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/sass/sass-original.svg" alt="sass" width="40" height="40"/> </a> <a href="https://tailwindcss.com/" target="_blank"> <img src="https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg" alt="tailwind" width="40" height="40"/> </a> <a href="https://www.typescriptlang.org/" target="_blank"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg" alt="typescript" width="40" height="40"/> </a> <a href="https://unity.com/" target="_blank"> <img src="https://www.vectorlogo.zone/logos/unity3d/unity3d-icon.svg" alt="unity" width="40" height="40"/> </a> </p>
@@ -55,13 +45,8 @@ const Linwaru = {
     <img align="left" src="https://github-readme-stats.vercel.app/api?username=linwaru&theme=tokyonight"><img align="right" src="https://github-readme-stats.vercel.app/api/top-langs/?username=linwaru&theme=tokyonight&hide=batchfile">
     <img src="https://github-readme-streak-stats.herokuapp.com/?user=linwaru&theme=tokyonight">
 </details>
-<!-- Last updated on ${localDate.toString()} -->
-<i>Last updated on ${localDate.getDate()}${localDate.getDate() === 1 ? "st" : localDate.getDate() === 2 ? "nd" : localDate.getDate() === 3 ? "rd" : "th"} ${["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"][localDate.getMonth()]} ${localDate.getFullYear()} at ${localDate.getHours()}:${localDate.getMinutes().toString().padStart(2, '0')}:${localDate.getSeconds().toString().padStart(2, '0')} using magic</i> ✨`;
+<!-- Last updated on ${brazilianTime.toString()} -->
+<i>Last updated on ${brazilianTime.getDate()}${brazilianTime.getDate() === 1 ? "st" : brazilianTime.getDate() === 2 ? "nd" : brazilianTime.getDate() === 3 ? "rd" : "th"} ${["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"][brazilianTime.getMonth()]} ${brazilianTime.getFullYear()} at ${brazilianTime.getHours()}:${brazilianTime.getMinutes().toString().padStart(2, '0')}:${brazilianTime.getSeconds().toString().padStart(2, '0')} using magic</i> ✨`;
 
-    if (readmeContent !== text) {
-        console.log('Updating README.md...');
-        fs.writeFileSync(ReadMe, text);
-    } else {
-        console.log('No changes detected, skipping update...');
-    }
+    fs.writeFileSync(ReadMe, text);
 })();
